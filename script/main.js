4ff42a67b07048f7a11ac13ed09282cfa33c1f32
sneharatnani <sneharatnani5@gmail.com>
2022-01-05 17:58:38 +0530

    added rainbow mode and general style

--- script/main.js
+++ script/main.js
@@ -3,71 +3,97 @@ let container = document.querySelector('.container');
 // to enable standard mode
 let standardBtn = document.querySelector('.standard');
 // to enable colorful mode
-let rainbow = document.querySelector('.rainbow');
+let RainbowBtn = document.querySelector('.rainbow');
 // to reset grid
 let reset = document.querySelector('.reset');
-// generate random rgb color
 
 
-
-// default function to generate grid
-function getGrid(boxes, color) {
+// to generate grid
+function getGrid(boxes, funcName) {
     for (let i = 0; i < boxes * boxes; i++) {
         let div = document.createElement('div');
         container.appendChild(div);
 
-        div.addEventListener('mouseover', (e) => {
-            div.style.background = color;
-        });
+        div.addEventListener('mouseover', funcName);
     }
     container.setAttribute('style', `display: grid; grid-template-columns: repeat(${boxes}, 1fr); grid-template-rows: repeat(${boxes}, 1fr)`);
 }
-getGrid(16, 'black');
+
+
+// adjust grid according to the input
+function adjustGrid(funcName) {
+    let boxes = prompt('Enter grid size between 1-100', 16);
+    if (boxes > 100) {
+        alert('oh very large number. Try again!');
+        getGrid(16, funcName);
+    }
+    else if (boxes === null || boxes === '') {
+        getGrid(16, funcName);
+    }
+    else {
+        getGrid(boxes, funcName);
+    }
+}
+
+/* functions for event listener */
+
+// default function
+function makeDefault(e) {
+    e.target.style.background = 'black';
+}
+// for rainbow mode
+function giveRainbow(e) {
+    e.target.style.background = `rgb(${getColor()}, ${getColor()}, ${getColor()})`;
+}
+
+getGrid(16, makeDefault);
 
 
 // reset grid
 function resetGrid() {
-    /*   while (container.lastChild) {
-          container.removeChild(container.lastChild);
-      } */
     container.textContent = '';
 }
 
-// standard function whith grid adjustment
-function getStandard() {
-    let boxes = prompt('Enter grid size between 1-100', 16);
-    if (boxes > 100) {
-        alert('oh very large number. Try again!');
-        getGrid(16, 'black');
-    }
-    else if (boxes === null || boxes === '') {
-        getGrid(16, 'black');
-    }
-    else {
-        getGrid(boxes, 'black');
-    }
+// to make random color
+function getColor() {
+    let color = Math.floor(Math.random() * 255);
+    return color;
 }
 
-// click event on standard button
+
+// events
 standardBtn.addEventListener('click', () => {
     resetGrid();
-    getStandard();
+    adjustGrid(makeDefault);
 });
 
-// click event on reset button
 reset.addEventListener('click', () => {
     resetGrid();
-    getGrid(16, 'black');
+    getGrid(16, makeDefault);
 });
 
-let rgb01 = () => Math.floor(Math.random() * 255);
-let rgb02 = () => Math.floor(Math.random() * 255);
-let rgb03 = () => Math.floor(Math.random() * 255);
-
-rainbow.addEventListener('click', () => {
+RainbowBtn.addEventListener('click', () => {
     resetGrid();
-    getGrid(16, `rgb(${rgb01()},${rgb02()},${rgb03()})`);
+    adjustGrid(giveRainbow);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
